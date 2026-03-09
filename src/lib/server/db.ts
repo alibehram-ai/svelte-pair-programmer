1fbe8e539ed4e46331dcd1553fdcc9d14b334a77
alibehram-ai <ali999.sc@gmail.com>
2026-03-09 08:31:46 +0500

    fixes for prisma adapter

--- src/lib/server/db.ts
+++ src/lib/server/db.ts
@@ -1,13 +1,19 @@
-import { PrismaClient } from "../../generated/prisma";
+import { PrismaClient } from "$lib/../generated/prisma/client";
+import { PrismaLibSql } from "@prisma/adapter-libsql";
 import { dev } from "$app/environment";
 
 const globalForPrisma = globalThis as unknown as {
   prisma?: PrismaClient;
 };
 
+const adapter = new PrismaLibSql({
+  url: "file:./prisma/dev.db",
+});
+
 export const prisma =
   globalForPrisma.prisma ??
   new PrismaClient({
+    adapter,
     log: ["error", "warn"],
   });
 
